11a812ceaf4867890d70f660c76f74da5b33297c
Jagshish Singh <singhjagshish0001@gmail.com>
2021-05-29 17:48:20 +0530

    navbar added with ant design UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
+import Header from './components/nav/Header';
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
-import {Home,Login,Register} from './pages';
+import { Switch, Route } from 'react-router-dom';
+import { Home, Login, Register } from './pages';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
-      
-
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </>
   );
 }
 
