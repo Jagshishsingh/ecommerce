bd589ff575cf6c9737e675d756ab3fcd209c9342
Jagshish Singh <singhjagshish0001@gmail.com>
2021-05-29 18:41:29 +0530

    firebase added and configured

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzZ-YiKTzsF8y_MO-0F3h65WzwW0KGCes",
+    authDomain: "ecommerce-9d27b.firebaseapp.com",
+    projectId: "ecommerce-9d27b",
+    storageBucket: "ecommerce-9d27b.appspot.com",
+    messagingSenderId: "956139818163",
+    appId: "1:956139818163:web:15aa88eb32e0a55a9ab4c6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth();
+
+  export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
