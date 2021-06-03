d939dbd2b66a175297268aed47c4747a5e6b0fcc
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-03 19:22:20 +0530

    redirection timer added

---
+++ client/src/components/routes/LoadingToRedirect.jsx
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+
+const LoadingToRedirect = () => {
+    const [count, setCount] = useState(5);
+    const history = useHistory();
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+          setCount(currentCount=>--currentCount);
+      }, 1000);
+      count === 0 && history.push('/');
+      return () => clearInterval(interval);
+    }, [count]);
+
+
+    return (
+        <div className="container p-5 text-center">
+            <p>REdirecting you in {count} seconds</p>
+        </div>
+    )
+}
+
+export default LoadingToRedirect
--- client/src/components/routes/UserRoute.js
+++ client/src/components/routes/UserRoute.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Route} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import LoadingToRedirect from './LoadingToRedirect';
 
 const UserRoute = ({children,...rest}) => {
     const {user} = useSelector(state => ({...state}));
 
     return user && user.token ? (<Route {...rest} render = {() => children} />)
-    : <h1>L</h1>;
+    : <LoadingToRedirect></LoadingToRedirect>;
 }
 
 export default UserRoute
