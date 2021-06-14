29c4f989572d4b8c19b83ec437b122db19f06419
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-14 21:41:02 +0530

    product remove route and controller

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {create, listAll} = require('../controllers/product');
+const {create, listAll, remove} = require('../controllers/product');
 const { authCheck, adminCheck } = require('../middlewares/auth');
 
 router.post('/product',authCheck,adminCheck,create);
 router.get("/products/:count", listAll); // products/100
+router.delete("/product/:slug", authCheck, adminCheck, remove);
 
 module.exports = router;
