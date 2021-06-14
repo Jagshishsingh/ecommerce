a64e12438785b85c1fcfbda94762b18ff2b7edbc
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-14 22:02:57 +0530

    product deletion functionality added

--- client/src/pages/admin/product/AllProducts.jsx
+++ client/src/pages/admin/product/AllProducts.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { getProductsByCount } from "../../../functions/product";
 import AdminProductCard from "../../../components/cards/AdminProductCard";
+import { removeProduct } from "../../../functions/product";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
     loadAllProducts();
@@ -24,6 +29,23 @@ const AllProducts = () => {
       });
   };
 
+  const handleRemove = (slug) => {
+    // let answer = window.confirm("Delete?");
+    if (window.confirm("Delete?")) {
+      // console.log("send delete request", slug);
+      removeProduct(slug, user.token)
+        .then((res) => {
+          loadAllProducts();
+          toast.error(`${res.data.title} is deleted`);
+        })
+        .catch((err) => {
+          if (err.response.status === 400) toast.error(err.response.data);
+          console.log(err);
+        });
+    }
+  };
+
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -40,7 +62,7 @@ const AllProducts = () => {
           <div className="row">
             {products.map((product) => (
               <div key={product._id} className="col-md-4 pb-3">
-                <AdminProductCard product={product} />
+                <AdminProductCard product={product} handleRemove={handleRemove} />
               </div>
             ))}
           </div>
