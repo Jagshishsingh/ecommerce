88081ca84b84cb726cfb80abd7d06dd218872c92
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-23 20:57:30 +0530

    added description tabs

--- client/src/components/cards/SingleProduct.jsx
+++ client/src/components/cards/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Tabs } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { Carousel } from "react-responsive-carousel";
@@ -8,40 +8,49 @@ import Laptop from '../../images/laptop.png';
 import ProductListItems from "./ProductListItems";
 
 
-const { Meta } = Card;
+const { TabPane } = Tabs;
 
 const SingleProduct = ({ product }) => {
-  const { title, images } = product;
+    const { title, images, description } = product;
 
-  return (
-    <>
-      <div className="col-md-7">
-        {images && images.length ? (
-          <Carousel showArrows={true} autoPlay infiniteLoop>
-            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
-          </Carousel>
-        ) : (
-          <Card cover={<img src={Laptop} className="mb-3 card-image" />}></Card>
-        )}
-      </div>
+    return (
+        <>
+            <div className="col-md-7">
+                {images && images.length ? (
+                    <Carousel showArrows={true} autoPlay infiniteLoop>
+                        {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+                    </Carousel>
+                ) : (
+                    <Card cover={<img src={Laptop} className="mb-3 card-image" />}></Card>
+                )}
+                <Tabs type="card">
+                    <TabPane tab="Description" key="1">
+                        {description && description}
+                    </TabPane>
+                    <TabPane tab="More" key="2">
+                        Call us on xxxx xxx xxx to learn more about this product.
+                    </TabPane>
+                </Tabs>
+            </div>
 
-      <div className="col-md-5">
-        <Card
-          actions={[
-            <>
-              <ShoppingCartOutlined className="text-success" /> <br />
-              Add to Cart
-            </>,
-            <Link to="/">
-              <HeartOutlined className="text-info" /> <br /> Add to Wishlist
-            </Link>,
-          ]}
-        >
-        <ProductListItems product={product} />
-        </Card>
-      </div>
-    </>
-  );
+            <div className="col-md-5">
+                <h1 className="bg-info p-3">{title}</h1>
+                <Card
+                    actions={[
+                        <>
+                            <ShoppingCartOutlined className="text-success" /> <br />
+                            Add to Cart
+                        </>,
+                        <Link to="/">
+                            <HeartOutlined className="text-info" /> <br /> Add to Wishlist
+                        </Link>,
+                    ]}
+                >
+                    <ProductListItems product={product} />
+                </Card>
+            </div>
+        </>
+    );
 };
 
 export default SingleProduct;
