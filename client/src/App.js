c45486052fceed19a6ae2e1faed3162a067907e3
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-24 20:29:59 +0530

    category and sub category individual pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { Home, Login, Register,RegisterComplete,ForgotPassword,Product } from './pages';
+import { Home, Login, Register, RegisterComplete, ForgotPassword, Product } from './pages';
 import Header from './components/nav/Header';
-import {auth} from './firebase';
+import { auth } from './firebase';
 import { currentUser } from './functions/auth';
 import UserRoute from './components/routes/UserRoute';
 import AdminRoute from './components/routes/AdminRoute';
-import {History,Wishlist,Password} from './pages/user';
-import {AdminDashboard,CategoryCreate,CategoryUpdate,
-  SubCreate, SubUpdate,ProductCreate,AllProducts,
+import { History, Wishlist, Password } from './pages/user';
+import {
+  AdminDashboard, CategoryCreate, CategoryUpdate,
+  SubCreate, SubUpdate, ProductCreate, AllProducts,
   ProductUpdate
-  } from './pages/admin';
+} from './pages/admin';
+import CategoryHome from "./pages/category/CategoryHome";
+import SubHome from "./pages/sub/SubHome";
+
 
 
 
@@ -24,25 +28,25 @@ function App() {
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if(user){
+      if (user) {
         const idTokenResult = await user.getIdTokenResult();
 
         // /current-user
         currentUser(idTokenResult.token)
-        .then(res=>{
+          .then(res => {
             dispatch({
-                type: "LOGGED_IN_USER",
-                payload: {
-                    name:res.data.name,
-                    email: res.data.email,
-                    token: idTokenResult.token,
-                    role:res.data.role,
-                    _id : res.data._id
-                }
+              type: "LOGGED_IN_USER",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data._id
+              }
             });
-        }).catch(error=>{
+          }).catch(error => {
             console.log(error);
-        });
+          });
 
       }
     });
@@ -51,17 +55,17 @@ function App() {
   return (
     <>
       <Header />
-      <ToastContainer/>
+      <ToastContainer />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
         <Route exact path='/register/complete' component={RegisterComplete} />
         <Route exact path='/forgot/password' component={ForgotPassword} />
-        <UserRoute exact path='/user/history'component = {History}/>
-        <UserRoute exact path='/user/wishlist'component = {Wishlist}/>
-        <UserRoute exact path='/user/password'component = {Password}/>
-        <AdminRoute exact path='/admin/dashboard'component = {AdminDashboard}/>
+        <UserRoute exact path='/user/history' component={History} />
+        <UserRoute exact path='/user/wishlist' component={Wishlist} />
+        <UserRoute exact path='/user/password' component={Password} />
+        <AdminRoute exact path='/admin/dashboard' component={AdminDashboard} />
         <AdminRoute exact path="/admin/category" component={CategoryCreate} />
         <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate} />
         <AdminRoute exact path="/admin/sub" component={SubCreate} />
@@ -71,6 +75,9 @@ function App() {
         <AdminRoute exact path="/admin/product/:slug" component={ProductUpdate} />
 
         <Route exact path='/product/:slug' component={Product} />
+        <Route exact path="/category/:slug" component={CategoryHome} />
+        <Route exact path="/sub/:slug" component={SubHome} />
+
       </Switch>
     </>
   );
