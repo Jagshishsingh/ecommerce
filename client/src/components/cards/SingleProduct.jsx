6eb2fcbdd2be048ed2fbbe7fccc1a227a72e452f
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-24 15:02:48 +0530

    rating added in front end

--- client/src/components/cards/SingleProduct.jsx
+++ client/src/components/cards/SingleProduct.jsx
@@ -11,7 +11,7 @@ import RatingModal from "../modal/RatingModal";
 
 const { TabPane } = Tabs;
 
-const SingleProduct = ({ product }) => {
+const SingleProduct = ({ product, onStarClick, star }) => {
     const { title, images, description, _id } = product;
 
     return (
@@ -49,10 +49,8 @@ const SingleProduct = ({ product }) => {
                         <StarRating
                           name={_id}
                           numberOfStars={5}
-                          rating={2}
-                          changeRating={(newRating, name) =>
-                            console.log("newRating", newRating, "name", name)
-                          }
+                          rating={star}
+                          changeRating={onStarClick}
                           isSelectable={true}
                           starRatedColor="red"
                         />
--- client/src/functions/product.jsx
+++ client/src/functions/product.jsx
@@ -35,3 +35,14 @@ export const getProducts = async (sort, order, limit) =>
   });
 export const getProductsCount = async () =>
   await axios.get(`${process.env.REACT_APP_API}/products/total`);
+
+export const productStar = async (productId, star, authtoken) =>
+  await axios.put(
+    `${process.env.REACT_APP_API}/product/star/${productId}`,
+    { star },
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
