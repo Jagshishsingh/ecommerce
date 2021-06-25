aa4939df6ace522242a407ebf4bb1adcd041b5ad
Jagshish Singh <singhjagshish0001@gmail.com>
2021-06-25 09:32:02 +0530

    redux setup for search

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import {userReducer} from './userReducer';
+import { searchReducer } from "./searchReducer";
 
 export const rootReducer = combineReducers({
-    user:userReducer
+    user:userReducer,
+    search: searchReducer,
 });
