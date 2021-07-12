da5ea085bd33a6315dd34706e61add1359267be3
Jagshish Singh <singhjagshish0001@gmail.com>
2021-07-12 19:46:34 +0530

    items can be to cart

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
 import {userReducer} from './userReducer';
 import { searchReducer } from "./searchReducer";
+import { cartReducer } from "./cartReducer";
 
 export const rootReducer = combineReducers({
     user:userReducer,
     search: searchReducer,
+    cart: cartReducer,
+
 });
