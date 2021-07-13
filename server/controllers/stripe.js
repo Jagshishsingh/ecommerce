b67c62d29c01e46345ac6c8dbf652e2b53134523
Jagshish Singh <singhjagshish0001@gmail.com>
2021-07-13 19:37:25 +0530

    stripe payment with discount

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,15 +5,37 @@ const Coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+  const { couponApplied } = req.body;
+
   // later apply coupon
   // later calculate price
 
+  // 1 find user
+  const user = await User.findOne({ email: req.user.email }).exec();
+  // 2 get user cart total
+  const { cartTotal, totalAfterDiscount } = await Cart.findOne({
+    orderdBy: user._id,
+  }).exec();
+  // console.log("CART TOTAL", cartTotal, "AFTER DIS%", totalAfterDiscount);
+
+  let finalAmount = 0;
+
+  if (couponApplied && totalAfterDiscount) {
+    finalAmount = totalAfterDiscount * 100;
+  } else {
+    finalAmount = cartTotal * 100;
+  }
+
+  // create payment intent with order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: finalAmount,
     currency: "usd",
   });
 
   res.send({
     clientSecret: paymentIntent.client_secret,
+    cartTotal,
+    totalAfterDiscount,
+    payable: finalAmount,
   });
 };
